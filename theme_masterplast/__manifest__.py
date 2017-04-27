c905d6f9230f3fd9470798e4c2915973ee79b79a
Alexsandro Haag <alex@hgsoft.com.br>
2017-04-27 14:13:15 -0300

    Acrescentadas customizações de Template iniciais, com base no bootswatch, para Masterplast

---
+++ theme_masterplast/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name':'Masterplast themes',
+  'description': 'Theme developed to Masterplast Fitas, based on Clean Theme.',
+  'version':'10.1.0',
+  'author':'Alexsandro Haag',
+  'data': [
+        'data/theme_masterplast_data.xml',
+        'views/theme_masterplast_templates.xml',
+    ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+  'images': ['static/description/masterplast.png'],
+  'application': False,
+  }
