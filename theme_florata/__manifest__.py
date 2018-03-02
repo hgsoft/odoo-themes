f65e1d9dd3bfbfc74b224679e4180a924f168123
Ewerton Rover <ewerton@hgsoft.com.br>
2018-03-02 18:04:34 -0300

    [FIX] - Ajustado layout e barra superior do layout reset_password.

--- theme_florata/__manifest__.py
+++ theme_florata/__manifest__.py
@@ -33,7 +33,7 @@
     
     'version': '1.0',
 
-    'depends': ['base', 'website_sale', 'website_anonymous_hide_prices', 'br_website_sale'],
+    'depends': ['base', 'website_sale', 'website_anonymous_hide_prices', 'br_website_sale', 'web'],
 
     'data': [
         # 'security/ir.model.access.csv',
